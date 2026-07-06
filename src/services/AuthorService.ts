e504e8639310705667157f31f9b39cf16081eb45
Brrn91 <lucasbruno81@gmail.com>
2026-07-06 18:13:25 -0300

    feat: adiciona findAll e findById ao AuthorService

--- src/services/AuthorService.ts
+++ src/services/AuthorService.ts
@@ -3,9 +3,17 @@ import { Author } from "../models/Author";
 
 export class AuthorService {
   private repository = new AuthorRepository();
-  
+
   async create(name: string, nationality: string, birthDate: string) {
     const author = new Author(name, nationality, birthDate);
     return await this.repository.create(author);
+  }
+
+  async findAll() {
+    return await this.repository.findAll();
+  }
+
+  async findById(id: number) {
+    return await this.repository.findById(id);
   }
 }
