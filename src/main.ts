6d76e7541fb178b3c3d51f97e79961e6c34b8c00
Brrn91 <lucasbruno81@gmail.com>
2026-07-07 19:09:36 -0300

    feat: liga authorMenu ao main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { testConnection } from "./database/connection";
+import { authorMenu } from "./menus/authorMenu";
 
 async function main(): Promise<void> {
   console.log("Iniciando BookStore Manager CLI");
   await testConnection();
+  await authorMenu();
 }
 
 main();
