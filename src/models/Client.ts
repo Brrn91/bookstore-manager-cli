dbc3e0daabd4ce9c998f0195bc84028d3b47f844
Brrn91 <lucasbruno81@gmail.com>
2026-07-07 20:32:46 -0300

    feat: cria model de Client

---
+++ src/models/Client.ts
@@ -0,0 +1,13 @@
+export class Client {
+  name: string;
+  phone: string;
+  email: string;
+  id?: number;
+
+  constructor(name: string, phone: string, email: string, id?: number) {
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+    this.id = id;
+  }
+}
