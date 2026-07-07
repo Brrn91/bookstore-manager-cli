0ccf8f2b44a77912b889c2758181a0745c4f30b3
Brrn91 <lucasbruno81@gmail.com>
2026-07-07 19:25:47 -0300

    feat: adiciona create ao BookRepository

---
+++ src/repositories/BookRepository.ts
@@ -0,0 +1,11 @@
+import { pool } from "../database/connection";
+import { Book } from "../models/Book";
+
+export class BookRepository {
+  async create(book: Book) {
+    await pool.query(
+      "INSERT INTO books (title, author_id, genre, published_year, total_copies, available_copies) VALUES ($1, $2, $3, $4, $5, $6)",
+      [book.title, book.authorId, book.genre, book.publishedYear, book.totalCopies, book.availableCopies],
+    );
+  }
+}
