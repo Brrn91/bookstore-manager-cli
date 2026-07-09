62640be1d5ca6592e21c982d9c3ae1488941fb5e
Brrn91 <lucasbruno81@gmail.com>
2026-07-09 17:13:03 -0300

    feat: cria LoanController completo

---
+++ src/controllers/LoanController.ts
@@ -0,0 +1,42 @@
+import { LoanService } from "../services/LoanService";
+import { ask, askNumber } from "../utils/inputHelper";
+
+export class LoanController {
+  private service = new LoanService();
+
+  async create() {
+    console.log("\n--- Cadastrar Empréstimo ---");
+    const bookId = await askNumber("ID do livro: ");
+    const clientId = await askNumber("ID do cliente: ");
+    const duoDate = await ask("Data prevista de devolução: ");
+    try {
+      await this.service.create(bookId, clientId, duoDate);
+      console.log("\n✅ Empréstimo cadastrado com sucesso!");
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  async list() {
+    console.log("\n--- Listando Empréstimos ---");
+    const listLoans = await this.service.findAll();
+    for (const loan of listLoans) {
+      console.log(`${loan.bookTitle} - ${loan.clientName} - ${loan.loanDate}`);
+    }
+    console.log("\n✅ Empréstimos listados com sucesso!");
+  }
+
+  async registerReturn() {
+    try {
+      const id = await askNumber("ID do Empréstimo: ");
+      const result = await this.service.registerReturn(id);
+      console.log("\n✅ Devolução registrada com sucesso!");
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  }
+}
